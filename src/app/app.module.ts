f039b6cab1b2ed0e880decdb087bd03d7447aa63
alfypro <alfredo.programador@gmail.com>
2019-11-06 23:47:19 +0100

    MERGE INIT

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { AppRoutingModule } from './/app-routing.module';
+import { LandingModule } from './landing/landing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    UiModule,
+    AppRoutingModule,
+    LandingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
