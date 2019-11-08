ae1ce0f548068871d706d3f95205d359b24a74e3
CONNECTIS-ICT\alfredo.rodrigo <alfredo.rodrigo@getronics.com>
2019-11-08 14:32:17 +0100

    Update

--- src/app/landing/home/home.component.ts
+++ src/app/landing/home/home.component.ts
@@ -1,41 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-const PROYECTOS = [
-    {
-        titulo: 'Gestion de Consentimiento GECO',
-        tipos: ['cipa', 'poblationalId', 'codint'],
-        alias: 'GECO'
-    },
-    {
-        titulo: 'Historia Social Resumida HSU',
-        tipos: ['cipa', 'docidentidad', 'patientid', 'codint'],
-        alias: 'HSU'
-    },
-    {
-        titulo: 'Test Sanitario SAMI',
-        tipos: ['cipa', 'poblationalId', 'codint'],
-        alias: 'SAMI'
-    }
-];
-
-interface Proyecto {
-    titulo: string;
-    tipos: string[];
-    alias: string;
-}
-
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    constructor() {}
 
-    proyectos: Proyecto[];
-
-    constructor() {
-        this.proyectos = PROYECTOS;
-    }
-
-    ngOnInit() { }
+    ngOnInit() {}
 }
--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -2,19 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { AssignmentComponent } from './assignment/assignment.component';
-import { HomeFormComponent } from './componentes/home-form/home-form.component';
-import { HomeTablaComponent } from './componentes/home-tabla/home-tabla.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports: [],
-  declarations: [
-    HomeComponent,
-    AssignmentComponent,
-    HomeFormComponent,
-    HomeTablaComponent
-  ]
+  declarations: [HomeComponent, AssignmentComponent]
 })
 export class LandingModule { }
